3fb30cc6705e88ab4b5edc7980994b1712b1fd01
prashanthsappidi <sappidiprashanth.12345@gmail.com>
2026-09-21 21:09:08 +0530

    21-sep-2026

--- src/test/java/day1/Demonstrate_different__selenium_locators.java
+++ src/test/java/day1/Demonstrate_different__selenium_locators.java
@@ -9,15 +9,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demonstrate_different__selenium_locators {
 
 	public static void main(String[] args) {
-		
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://testautomationpractice.blogspot.com/");
 		driver.manage().window().maximize();
 		driver.findElement(By.id("name")).sendKeys("Prashanth");
-		System.out.println(driver.findElements(By.className("form-control")).size());		//[class="form-control"]----> 7
-		System.out.println(driver.findElements(By.tagName("a")).size());                  // a[href]  ----> 33
+		System.out.println(driver.findElements(By.className("form-control")).size()); // [class="form-control"]----> 7
+		System.out.println(driver.findElements(By.tagName("a")).size()); // a[href] ----> 33
 		System.out.println(driver.findElement(By.linkText("Blog")).getText());
 		System.out.println(driver.findElement(By.partialLinkText("Udemy")).getText());
 		driver.findElement(By.cssSelector("[placeholder='Enter Phone']")).sendKeys("9908497995");
